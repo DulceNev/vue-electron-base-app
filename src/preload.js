b341d60faf4c2c05a3d1273db997ca528dcf03fa
Dulce Nevarez <dulcenev04@gmail.com>
2026-03-16 18:50:43 -0500

    Initialize a new Vue 3 Electron application with Vite and Electron Forge.

---
+++ src/preload.js
@@ -0,0 +1,2 @@
+// Los scripts de precarga contienen el código que se ejecuta en un proceso de renderizado antes de que su contenido web comience a cargar.
+// Estos scripts se ejecutan dentro del contexto del renderizador, pero tienen más privilegios al poder acceder a las APIs de Node.js.
